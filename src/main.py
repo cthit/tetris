ee238588e47a19e86b2938b4d578a7678c37bd13
x183 <palmoscar4@gmail.com>
2022-05-17 22:37:24 +0200

    Added button.py

--- src/main.py
+++ src/main.py
@@ -1,17 +1,21 @@
 import pygame
-
+from components.button import Button
 
 def main():
 	pygame.init()
 	sWidth=800
 	sHeight=600
 	screen=pygame.display.set_mode((sWidth,sHeight))
+	image=pygame.image.load(r'resources/images/logo.png')
 	isRunning=True
+	b=Button(1,"hej",(100,100))
 	while isRunning:
 		for event in pygame.event.get():
 			if event.type==pygame.QUIT:
 				isRunning=False
-		screen.fill((255,255,255))
+		screen.fill((0,0,0))
+		screen.blit(image,(200,100))
+		pygame.display.update()
 
 
 
