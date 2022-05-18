b4ec7af573dbe1ead24fa81994e52c4fc40f79ba
x183 <palmoscar4@gmail.com>
2022-05-18 22:47:44 +0200

    .

--- src/config.py
+++ src/config.py
@@ -1,9 +1,14 @@
-
+# Screen setup:
 HEIGHT = 600
 WIDTH = 800
 SCREENSIZE = (WIDTH, HEIGHT)
-BACKGROUND_COLOUR = (50, 50, 50)
 SCREEN_NAME = "\"TETRIS\""
-FONT= "comicsansms"
+
+# Font related shenanigans:
+STANDARD_FONT= "comicsansms"
 FONT_SIZE=20
-BUTTON_COLOR = (172,216,230)
+
+
+# Different standard colours:
+BACKGROUND_COLOUR = (50, 50, 50)
+BUTTON_COLOUR = (172,216,230)
