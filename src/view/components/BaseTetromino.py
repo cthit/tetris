c150f1f268ca49f91d64c5f415017a6a886cbc5f
x183 <palmoscar4@gmail.com>
2022-05-29 00:42:21 +0200

    Refactored BaseTetromino.py

--- src/view/components/BaseTetromino.py
+++ src/view/components/BaseTetromino.py
@@ -7,16 +7,25 @@ from controller.Observer import Observer
 class BaseTetromino(pygame.sprite.Sprite, Observer):
     def __init__(self, text, size, position):
         pygame.sprite.Sprite.__init__(self)
+        self.createImage()
+        self.createRect()
+        self.setupKeyboard()
+
+    def createImage(self):
         self.base_font=pygame.font.SysFont(STANDARD_FONT, FONT_SIZE)
         self.image = pygame.Surface(size)
         self.image.fill(BUTTON_COLOUR)
+        #Text is temporary
         self.text=self.base_font.render(text, True, (0, 0, 0))
         self.textrect=self.text.get_rect(center=self.image.get_rect().center)
         self.image.blit(self.text,self.textrect)
-        #pygame.draw.rect(self.image, (200, 200,200),pygame.Rect(position[0], position[1], size[0],size[1]))
+
+    def createRect(self):
         self.rect=self.image.get_rect()
         self.rect.x=position[0]
         self.rect.y=position[1]
+
+    def setupKeyboard(self):
         keyboard.on_press_key('a', self.left)
         keyboard.on_press_key('d', self.right)
         keyboard.on_press_key('s', self.down)
@@ -24,10 +33,13 @@ class BaseTetromino(pygame.sprite.Sprite, Observer):
 
     def down(self, event):
         self.rect.y=self.rect.y+10
+
     def left(self, event):
         self.rect.x=self.rect.x-10
+
     def right(self, event):
         self.rect.x=self.rect.x+10
+
     def rotate(self, event):
         posX=self.rect.x
         posY=self.rect.y
